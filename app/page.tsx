e39a7c9008d0f6c658a77d529d68e9be6a66afff
moenes <moenes.samp55@gmail.com>
2025-04-08 23:27:52 +0100

    Changed to TypeScript v2

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { Hero } from "../components/Hero";
 import MatchComponent from "../components/MatchComponent";
 import { LiveMatch } from "../components/LiveMatch";
-import {Wins} from "../components/Wins";
 import { Reviews } from "../components/Reviews";
 import NewsSection from "../components/News/NewsSection";
 import { ElitePerformance } from "../components/ElitePerformance";
 import Partners from "../components/Partners";
 import Articles from "@/components/Articles";
+import Wins from "@/components/Wins";
 
 export default function Home() {
   return (
--- components/Wins.tsx
+++ components/Wins.tsx
@@ -2,12 +2,17 @@ import Image from 'next/image';
 import * as images from './../assets/index';
 import { GiBabyfootPlayers } from 'react-icons/gi';
 
-export const Wins = () => {
+const Wins = () => {
   return (
     <div className="flex flex-col py-4 px-2 lg:px-12">
       <div className="container mx-auto flex flex-col lg:flex-row items-center gap-6">
-        <div className="lg:w-1/2">
-        <Image src={images.WORLDCUP.src} alt="WorldCup Image" width={500} height={300} />
+      <div className="lg:w-1/2">
+          <Image
+            src={images.WORLDCUP.src}
+            alt="WorldCup Image"
+            width={500}
+            height={300}
+          />
           <div className="px-9 pt-10 pb-14 bg-[#251d43]">
             <div className="text-white space-y-4">
               <h3 className="text-xl uppercase font-bold lead-xl bold">Argentina vs France</h3>
@@ -15,20 +20,25 @@ export const Wins = () => {
             </div>
             <div className="flex justify-between pt-8">
               <ul className="flex flex-col gap-y-2.5">
-                <li className="flex space-x-3 text-white">
-                  <GiBabyfootPlayers className="w-6 h-6" />
-                  <span className="paragraph-l font-bold">Lionel Messi '2 Goals'</span>
-                </li>
-                <li className="flex space-x-3 text-white">
-                  <GiBabyfootPlayers className="w-6 h-6" />
-                  <span className="paragraph-l font-bold">Angel Maria '1 Goal'</span>
-                </li>
+              <li className="flex space-x-3 text-white">
+  <GiBabyfootPlayers className="w-6 h-6" />
+  <span className="paragraph-l font-bold">Lionel Messi &apos;2 Goals&apos;</span>
+</li>
+<li className="flex space-x-3 text-white">
+  <GiBabyfootPlayers className="w-6 h-6" />
+  <span className="paragraph-l font-bold">Angel Maria &apos;1 Goal&apos;</span>
+</li>
               </ul>
             </div>
           </div>
         </div>
         <div className="lg:w-1/2 mt-6 lg:mt-0">
-        <Image src={images.ALNASR.src} alt="ALNASSR Image" width={500} height={300} />
+          <Image
+            src={images.ALNASR.src}
+            alt="ALNASSR Image"
+            width={500}
+            height={300}
+          />
           <div className="px-9 pt-10 pb-14 bg-[#251d43]">
             <div className="text-white space-y-4">
               <h3 className="text-xl uppercase font-bold lead-xl bold">Al Nasr vs Al Ahli Saudi</h3>
@@ -52,3 +62,5 @@ export const Wins = () => {
     </div>
   );
 };
+
+export default Wins;
